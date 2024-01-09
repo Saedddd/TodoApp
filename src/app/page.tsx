38cf5b858be2cdce02764fa0b4b0c6e044d3fec6
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-01-09 16:17:01 +0600

    Install daisyUI as a Tailwind CSS plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <main>
+        <h1 className="text-center underline">To Do app for learning Next.js 13</h1>
+    </main>
   )
 }

7145000f6fe56ad8a6ab721e0a7d3493efea31fb
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2024-01-09 16:35:02 +0600

    Add addTask component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-
+import AddTask from "@/features/AddTask"
 
 export default function Home() {
   return (
-    <main>
-        <h1 className="text-center underline">To Do app for learning Next.js 13</h1>
+    <main className="max-w-4xl mx-auto mt-4">
+        <div className="text-center my-5 flex flex-col gap-4">
+          <h1 className="text-2xl font-bold"> ToDo list app for learning next 14</h1>
+          <AddTask />
+        </div>
     </main>
   )
 }
